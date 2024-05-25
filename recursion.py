3986cb4fcc2c8637fff58ce5aefed122e7303e75
letmediealrdy <letmediealrdy@gmail.com>
2024-05-25 02:21:08 +0200

    initial

--- recursion.py
+++ recursion.py
@@ -1,10 +1,12 @@
-def test(a = 1, b = True):
-    print(a, b)
-test()
-def rec(*n):
-    for i in n:
-        if i <= 1:
-            return i
-        else:
-            return i * rec(i-1)
+def test(*a, **k):
+    for i in a:
+        print(i)
+    for v in k.items():
+        print(v)
+test(1, True, 'string', **{'D': '1000'})
+def rec(n):
+    if n <= 1:
+        return n
+    else:
+        return n * rec(n-1)
 print(rec(int(input('Введите число: '))))
